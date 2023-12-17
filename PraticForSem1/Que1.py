3582e52c98ba2bf3572fc8c9487da8cce58517fa
YashAdhau9211 <143120526+YashAdhau9211@users.noreply.github.com>
2023-12-17 13:57:31 +0000

    que 1 done

---
+++ PraticForSem1/Que1.py
@@ -0,0 +1,6 @@
+name = input("Enter your Name: ")
+age = int(input("Enter your Age: "))
+
+age_after_100 = ( 2023 - age ) + 100
+
+print(f"Yaour name is {name} is and your 100th birthday will be on {age_after_100} year.")
